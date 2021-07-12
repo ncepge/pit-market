f66d6e580fe0f554dd9d4f35ebc9690dbd916da5
Clyff <clyfford_robert@hotmail.com>
2021-07-12 16:19:30 -0300

    realiza traducao em public/scripts/player/lobby.js

--- public/scripts/player/lobby.js
+++ public/scripts/player/lobby.js
@@ -5,7 +5,7 @@ function getContext(id) { return document.getElementById(id); }
 function defaultText()
 {
 	div = getContext("help_text");
-	var html = "<h1>First Time?</h1><p>Use os bot&otilde;es abaixo para aprender mais sobre o jogo</p>";
+	var html = "<h1>Primeira vez?</h1><p>Use os bot&otilde;es abaixo para aprender mais sobre o jogo</p>";
 	div.innerHTML = html;
 }
 function about()
@@ -28,19 +28,13 @@ function rules()
 	div = getContext("help_text");
 
 	// Crete html
-	var html = `
-<h1>Regras</h1>
-<p>No in&iacute;cio de cada rodada, voc&ecirc; receber&aacute; uma carta com um valor.\
-Voc&ecirc; tamb&eacute;m ser&aacute; informado se ser&aacute; um Comprador ou Vendedor. \
-
-Se voc&ecirc; &eacute; um Vendedor, seu objetivo &eacute; vender seus "produtos" por um pre&ccedil;o superior ao valor do seu cart&atilde;o. \
-
-Se voc&ecirc; &eacute; um Comprador, seu objetivo &eacute; comprar "mercadorias" por um pre&ccedil;o inferior ao valor do seu cart&atilde;o. \
-
-A diferen&ccedil;a entre o valor do sua carta e o pre&ccedil;o pelo qual voc&ecirc; compra ou vende &eacute; o seu lucro. \
-Para fazer um acordo com outro jogador, voc&ecirc; deve publicar uma oferta ou aceitar. Postar uma oferta pode ser feito inserindo uma oferta legal em seu dispositivo e pressionando ‘Enviar’. Voc&ecirc; tamb&eacute;m ver&aacute; ofertas de outros jogadores que aparecem em seu dispositivo. \
-Se voc&ecirc; deseja aceitar uma dessas ofertas, basta clicar na oferta. \ 
-</p>`
+	var html = "<h1>Regras</h1>";
+	html += "<p>No in&iacute;cio de cada <b>rodada</b>, voc&ecirc; receber&aacute; uma <b>carta</b> com um valor. Voc&ecirc; tamb&eacute;m ser&aacute; informado se ser&aacute; um <b>Comprador</b> ou <b>Vendedor</b>.</p>";
+	html += "<p>Se voc&ecirc; &eacute; um <b>Vendedor</b>, seu objetivo &eacute; vender seus \"produtos\" por um pre&ccedil;o superior ao valor do seu cart&atilde;o.</p>"
+	html += "<p>Se voc&ecirc; &eacute; um <b>Comprador</b>, seu objetivo &eacute; comprar \"mercadorias\" por um pre&ccedil;o inferior ao valor do seu cart&atilde;o.</p>"
+	html += "<p>A diferen&ccedil;a entre o valor do sua carta e o pre&ccedil;o pelo qual voc&ecirc; compra ou vende &eacute; o seu <b>lucro</b>.</p>"
+	html += "<p>Para fazer um <b>acordo</b> com outro jogador, voc&ecirc; deve <b>publicar</b> uma <b>oferta</b> ou <b>aceitar</b>. Postar uma oferta pode ser feito inserindo uma oferta legal em seu dispositivo e pressionando ‘Enviar’. Voc&ecirc; tamb&eacute;m ver&aacute; ofertas de outros jogadores que aparecem em seu dispositivo.</p>"
+	html += "<p>Se voc&ecirc; deseja aceitar uma dessas ofertas, basta clicar na oferta.</p>"
 
 	// html += "<p>Every player will be assigned a card at random. This card will tell you whether you are buying or selling, and what your card is worth.</p>"
 	// html += "<p><b>Buyers</b> are unable to buy a card for more than their card's value<br/>";
@@ -59,7 +53,7 @@ function how()
 	div = div = getContext("help_text");
 
 	// Crete html
-	var html = "<h1>How to play the game</h1>"
+	var html = "<h1>Como jogar o game</h1>"
 	html += "TODO"
 	// Set the html
 	div.innerHTML = html;
@@ -72,21 +66,21 @@ function special()
 
 	// Crete html
 	var html = "<h1>Regras Especiais</h1>"
-	html += "<p> The host of the game may introduce special rules for a round or for a whole game. Any special rules will be displayed on the screen during the round and on the lobby screen.<p>"
+	html += "<p>O anfitri&atilde;o do jogo pode introduzir regras especiais para uma rodada ou para um jogo inteiro. Quaisquer regras especiais ser&atilde;o exibidas na tela durante a rodada e na tela do lobby.<p>"
 	
 	// Tax
-	html += "<h2>Tax</h2>";
-	html += "<p><b>Tax</b> will be a flat cost that the <emp>seller</emp> must pay when they successfully sell a card."
-	html += " For example;</p><p style='text-indent :1em;'><p>" + "If a card of value $10 is sold for $15 and there is currently a $2 tax in place, the seller will have $5 of before-tax profit, then after paying the $2 tax will have a net gain of $3 for the round</p>";
-	html += "<p>Sale Value - Card Value - Tax = Profit<br />"
-	html += "$15 - $10 - $2 = $3<p>"
-	html += "<p>Tax is a real-world phenomenon that governments impose on the sale of goods and services in everyday life and this rule will demonstrate how tax may affect the free market."
+	html += "<h2>Imposto</h2>";
+	html += "<p>O <b>imposto</b> ser&aacute; um custo fixo que o <emp>vendedor</emp> dever&aacute; pagar ao vender um carta com sucesso."
+	html += " Por exemplo;</p><p style='text-indent :1em;'><p>" + "Se um cart&atilde;o de valor de $10 for vendido por $15 e houver atualmente um imposto de $2 em vigor, o vendedor ter&aacute; $5 de lucro antes dos impostos e, depois de pagar o imposto de $2, ter&aacute; um ganho l&iacute;quido de $3 para a rodada.</p>";
+	html += "<p>Valor de venda - Valor da carta - Imposto = lucro<br />"
+	html += "$ 15 - $ 10 - $ 2 = $ 3<p>"
+	html += "<p>O imposto &eacute; um fen&ocirc;meno do mundo real que os governos imp&otilde;em sobre a venda de bens e servi&ccedil;os na vida cotidiana e esta regra ir&aacute; demonstrar como o imposto pode afetar o mercado livre."
 	// Price ceiling/floor
-	html += "<h2>Price Ceiling/Floor</h2>";
-	html += "<p>When there is either a price ceiling or floor in place, there is a limit to how much a card can be brought or sold at. A price floor sets a minimum value that a card can be sold at and a price ceiling reflects a maximum value. During any particular round, there will only be <emp>one</emp> of either option and never both.<p>"
-	html += "<p>The <b>floor</b> may reprent a minimum cost of production of the goods or services for a company."
-	html += "<br />The <b>ceiling</b> may represent a price cap enforced on seller by a government.</p>"
-	html += "<p>Note that <b>it is possible</b> that you will no longer be able to make a trade when this rule is in place. In the real world, compainies could be forced out of a market because of various policies or costs introduced.<p>"
+	html += "<h2>Pre&ccedil;o teto ou piso</h2>";
+	html += "<p>Quando h&aacute; um teto ou piso de pre&ccedil;o, h&aacute; um limite de quanto uma carta pode ser comprada ou vendida. Um pre&ccedil;o m&iacute;nimo define um valor m&iacute;nimo pelo qual um cart&atilde;o pode ser vendido e um pre&ccedil;o m&aacute;ximo reflete um valor m&aacute;ximo. Durante qualquer rodada em particular, haver&aacute; apenas <emp>uma</emp> das op&ccedil;&otilde;es e nunca ambas.<p>"
+	html += "<p>O <b>piso</b> pode representar um custo m&iacute;nimo de produ&ccedil;&atilde;o de bens ou servi&ccedil;os para uma empresa."
+	html += "<br />O <b>teto</b> pode representar um teto de pre&ccedil;o imposto ao vendedor por um governo.</p>"
+	html += "<p>Observe que <b>&eacute; poss&iacute;vel</b> que voc&ecirc; n&atilde;o consiga mais fazer uma negocia&ccedil;&atilde;o quando esta regra estiver em vigor. No mundo real, as empresas podem ser for&ccedil;adas a sair de um mercado devido a v&aacute;rias pol&iacute;ticas ou custos introduzidos.<p>"
 
 	// Set the html
 	div.innerHTML = html;	
